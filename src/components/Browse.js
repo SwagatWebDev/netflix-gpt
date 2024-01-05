c57f40176a0319c9d4bdfcf4d91c8e3fa479c80b
swagat030 <swagatkumarmishra888@gmail.com>
2024-01-05 19:45:36 +0530

    Enhancement made

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,20 @@
 import Header from "./Header";
+import {API_OPTION} from "../utils/constants";
+import {useEffect} from "react";
 
 export const Browse = () => {
+
+    const getNowPlayingMovies = async() => {
+        const data = await fetch(
+            'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTION);
+        const json = await data.json();
+        console.log(json.results);
+    }
+
+    useEffect(() => {
+        getNowPlayingMovies();
+    }, []);
+
     return (
         <div>
             <Header/>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,11 @@ export const HEADER_LOGO_URL = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-45
 export const LOGIN_LOGO_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/563192ea-ac0e-4906-a865-ba9899ffafad/6b2842d1-2339-4f08-84f6-148e9fcbe01b/IN-en-20231218-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
 export const USER_ICON_URL = "https://occ-0-5690-3663.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
+
+export const API_OPTION = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMDliNjYyNDZmMDhhMmFjYTJkYTA3ZDA1ODgyN2Y5ZiIsInN1YiI6IjY1OTc5ZTY1NWNjMTFkNzc2ZTdkOGE1MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pHVa-ZXG98A2X8_lbXXEwGYRX8d_zf2lq8XsxN4aKcA'
+    }
+};
